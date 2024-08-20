6ae4a3b40140b780c91bf5e540d5152049a21c2f
Navodplayer1 <navodpeiris1234@gmail.com>
2024-08-20 19:40:15 +0530

    fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ language = "el"          # language code
 log_folder = "logs"      # log folder for storing transcripts
 modelSize = "tiny"     # size of model to be used [tiny, small, medium, large-v1, large-v2, large-v3]
 quantization = False   # setting this 'True' may speed up the process but lower the accuracy
-ACCESS_TOKEN = "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" # get permission to access pyannote/speaker-diarization@2.1 on huggingface
+ACCESS_TOKEN = "your huggingface token" # get permission to access pyannote/speaker-diarization@2.1 on huggingface
 
 # quantization only works on faster-whisper
 transcriptor = Transcriptor(file, log_folder, language, modelSize, ACCESS_TOKEN, voices_folder, quantization)
